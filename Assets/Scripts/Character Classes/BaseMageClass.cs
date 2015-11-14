c3f6ee69e4ab184b682d1daea3e6755849aa68de
merlin18x <merlin18x@gmail.com>
2015-11-14 14:08:10 -0800

    First Commit

---
+++ Assets/Scripts/Character Classes/BaseMageClass.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseMageClass : BaseCharacterClass {
+	
+	public BaseMageClass()
+	{
+		CharacterClassName = "Mage";
+		CharacterClassDescription = "A smart and mystic mage";
+		
+		Stamina = 12;
+		Endurance = 10;
+		Strength = 10;
+		Intellect = 35;
+	}
+
+}
---
+++ Assets/Scripts/Character Classes/BaseWarriorClass.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseWarriorClass : BaseCharacterClass {
+
+	public BaseWarriorClass()
+	{
+		CharacterClassName = "Warrior";
+		CharacterClassDescription = "A powerful and strong warrior that clubs you over the head";
+
+		Stamina = 20;
+		Endurance = 24;
+		Strength = 20;
+		Intellect = 10;
+	}
+
+}
